4110e5c268f3f499d32376122bc0020881c56756
Facundo Bravo <facundobravot@gmail.com>
2023-09-02 01:03:28 -0300

    Comment on Gun. Script

--- Assets/_Scripts/Weapons/Old/Gun.cs
+++ Assets/_Scripts/Weapons/Old/Gun.cs
@@ -46,6 +46,7 @@ public class Gun : MonoBehaviour
             {
                 //NUEVO:
 
+                /*
                 if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance))
                 {
                     //Debug.Log(hit.transform.name); //Debug.Log(hit.point)
@@ -82,27 +83,11 @@ public class Gun : MonoBehaviour
 
                 }
 
-                /*VIEJO:
-
-                //Cambiar segun sea necesario el origen y direccion del disparo
-                //Calculos con la camara y Efectos con el muzzle
-                if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo, gunData.maxDistance))
-                {
-                    print(hitInfo.transform.name);
-                    IDamageable damageable = hitInfo.transform.GetComponent<IDamageable>();
-                    damageable?.TakeDamage(gunData.damage);
-
-                    //hit.rigidbody.AddForce(ray.direction * hitForce);
-                    //rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-                    playerRB.AddForce(-cam.forward * gunData.nockback, ForceMode.Impulse);
-                }
-                */
-
-                //OBLIGATORIO:
-
                 gunData.currentAmmo--;
                 timeSinceLastShot = 0;
                 OnGunShot();
+
+                */
 
             }
         }
@@ -111,8 +96,8 @@ public class Gun : MonoBehaviour
     private void Update()
     {
         timeSinceLastShot += Time.deltaTime;
-        Debug.DrawRay(cam.position, cam.forward * gunData.maxDistance, Color.blue);
-        Debug.DrawRay(muzzle.position, cam.forward * gunData.maxDistance, Color.red);
+        //Debug.DrawRay(cam.position, cam.forward * gunData.maxDistance, Color.blue);
+        //Debug.DrawRay(muzzle.position, cam.forward * gunData.maxDistance, Color.red);
 
     }
 
