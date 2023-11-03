3be86082d7b11f0e90f7397e2a7cee29c50dc7d1
Facundo Bravo <facundobravot@gmail.com>
2023-11-02 21:33:35 -0300

    BowCharacterShoot Script

--- Assets/_Scripts/Player/PlayableCharacters/BowCharacterShoot.cs
+++ Assets/_Scripts/Player/PlayableCharacters/BowCharacterShoot.cs
@@ -5,15 +5,95 @@ using UnityEngine;
 public class BowCharacterShoot : PlayerShoot
 {
     [Header("Bow Configuration")]
-    //[SerializeField] float shootForce;
+    [SerializeField] float shootForce;
+    private float currentShootForce;
+    [SerializeField] float shootChargeSpeed;
+    [SerializeField] float shootMaxCharge;
+    [SerializeField] float shootReloadTime;
+    [SerializeField] float chargeTime;
 
-    void Start()
+    [Header("Bow Feedback")]
+    [SerializeField] float moveSpeed = 10f;
+    private float moveDuration = 2.0f;
+    private RectTransform rectTransform;
+    private Vector3 initialPos;
+    private bool bowIsMoving = false;
+    private float moveTimer = 0.0f;
+
+    protected override void Start()
     {
-        
+        base.Start();
+
+        rectTransform = weaponObject.GetComponent<RectTransform>();
+        initialPos = rectTransform.position;
+        moveDuration = shootMaxCharge;
     }
 
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+        ShootInput();
+        Animation();
     }
+
+    private void ShootInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            chargeTime = 0;
+            moveTimer = 0.0f;
+            bowIsMoving = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            isCharging = true;
+            if (isCharging && !isReloading && chargeTime < shootMaxCharge)
+            {
+                chargeTime += Time.deltaTime * shootChargeSpeed;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            bowIsMoving = false;
+            rectTransform.position = initialPos;
+
+            if (chargeTime <= shootMaxCharge * 0.5) //No Shoot
+            {
+                chargeTime = 0;
+            }
+            else
+            if (chargeTime >= shootMaxCharge * 0.5 && chargeTime <= shootMaxCharge * 0.75) //Weak Shoot
+            {              
+                StartCoroutine(ShootProjectile(shootForce * 0.5f, shootReloadTime));
+                chargeTime = 0;
+            }
+            else
+            if (chargeTime >= shootMaxCharge * 0.75 && chargeTime <= shootMaxCharge + 1) //Hard Shoot
+            {
+                StartCoroutine(ShootProjectile(shootForce, shootReloadTime));
+                chargeTime = 0;
+            }
+        }
+    }
+
+    private void Animation()
+    {
+        if (bowIsMoving)
+        {
+            if (moveTimer < moveDuration)
+            {
+                float distance = rectTransform.sizeDelta.y * 0.5f;
+                float step = moveSpeed * Time.deltaTime;
+                Vector3 targetPosition = initialPos - new Vector3(0, distance, 0);
+                rectTransform.position = Vector3.MoveTowards(rectTransform.position, targetPosition, step);
+                moveTimer += Time.deltaTime;
+            }
+            else
+            {
+                bowIsMoving = false;
+            }
+        }
+    }
+
 }
--- Assets/_Scripts/Player/PlayerShoot.cs
+++ Assets/_Scripts/Player/PlayerShoot.cs
@@ -7,121 +7,44 @@ public class PlayerShoot : MonoBehaviour
 {
     [Header("Configuration")]
     public ForceMode forceMode;
-    
-    public float shootForce;
-    private float shootForceBackup;
 
-    [SerializeField] private float shootChargeSpeed;
-    [SerializeField] private float shootMaxCharge;
-    [SerializeField] private float shootReloadTime;
-
-    [Header("Debug")]
-    public bool isReloading;
-    private bool isCharging;
-    [SerializeField] private float chargeTime;
-
-    [Header("Bow Image")]
-    [SerializeField] float moveSpeed = 2.0f;
-    [SerializeField] float moveDuration = 2.0f;
-    [SerializeField] private RectTransform rectTransform;
-    private Vector3 initialPos;
-    private bool bowIsMoving = false;
-    private float moveTimer = 0.0f;
+    [Header("States")]
+    public bool isCharging = false;
+    public bool isWaiting = false;
+    public bool isReloading = false;
 
     [Header("Prefabs")]
     public Rigidbody projectilePrefab;
 
     [Header("References")]
-    [SerializeField] private GameObject bowObject;
+    public GameObject weaponObject;
     private Camera cam;
 
-    void Start()
+    protected virtual void Start()
     {
         cam = Camera.main;
-        if (bowObject == null) bowObject = GameObject.Find("BowTemplate");
-        rectTransform = bowObject.GetComponent<RectTransform>();
-        initialPos = rectTransform.position;
-
-        shootForceBackup = shootForce;
-        isReloading = false;
+        
+        if (weaponObject == null) weaponObject = GameObject.Find("WeaponHolder").transform.GetChild(0).gameObject;
     }
 
-    void Update()
+    protected virtual void Update()
     {
-        if (Input.GetMouseButtonDown(2));
-        if (Input.GetMouseButtonDown(1));
 
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            chargeTime = 0;
-            moveTimer = 0.0f;
-            bowIsMoving = true;
-        }
-        if (Input.GetMouseButton(0))
-        {
-            isCharging = true;
-            if (isCharging && !isReloading && chargeTime < shootMaxCharge)
-            {
-                chargeTime += Time.deltaTime * shootChargeSpeed;
-            }
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            bowIsMoving = false;
-            rectTransform.position = initialPos;
-
-            if (chargeTime <= shootMaxCharge * 0.5)
-            {
-                //print("No Shoot");
-                chargeTime = 0;
-            }
-            else
-            if (chargeTime >= shootMaxCharge * 0.5 && chargeTime <= shootMaxCharge * 0.75)
-            {
-                //print("Weak Shoot");
-                StartCoroutine(ShootProjectile(projectilePrefab, shootForce/2));
-            }
-            else
-            if (chargeTime >= shootMaxCharge * 0.75 && chargeTime <= shootMaxCharge + 1)
-            {
-                //print("Hard Shoot");
-                StartCoroutine(ShootProjectile(projectilePrefab, shootForce));
-            }
-        }
-
-        if (bowIsMoving)
-        {
-            if (moveTimer < moveDuration)
-            {
-                float distance = rectTransform.sizeDelta.y * 0.5f;
-                float step = moveSpeed * Time.deltaTime;
-                Vector3 targetPosition = initialPos - new Vector3(0, distance, 0);
-                rectTransform.position = Vector3.MoveTowards(rectTransform.position, targetPosition, step);
-                moveTimer += Time.deltaTime;
-            }
-            else
-            {
-                bowIsMoving = false;
-            }
-        }
     }
 
-    IEnumerator ShootProjectile(Rigidbody _projectilePrefab, float _shootForce)
+    public IEnumerator ShootProjectile(float _shootForce, float _shootReloadTime)
     {
         isCharging = false;
-        chargeTime = 0;
         isReloading = true;
 
         Vector3 spawnPosition = cam.transform.position;
         Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
         Quaternion rotation = Quaternion.LookRotation(ray.direction);
 
-        Rigidbody newProjectile = Instantiate(_projectilePrefab, spawnPosition, rotation);
+        Rigidbody newProjectile = Instantiate(projectilePrefab, spawnPosition, rotation);
         newProjectile.AddForce(ray.direction * _shootForce, forceMode);
 
-        yield return new WaitForSeconds(shootReloadTime);
-        shootForce = shootForceBackup;
+        yield return new WaitForSeconds(_shootReloadTime);
         isReloading = false;
     }
 }
