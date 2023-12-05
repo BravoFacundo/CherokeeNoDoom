98b411401c2dd1750b133fa7c23f2dbdca9b2a93
Facundo Bravo <facundobravot@gmail.com>
2023-12-05 18:54:45 -0300

    Update PlayerHUD Script - Dash Skill

--- Assets/_Scripts/Player/PlayerHUD.cs
+++ Assets/_Scripts/Player/PlayerHUD.cs
@@ -5,19 +5,8 @@ using UnityEngine;
 
 public class PlayerHUD : MonoBehaviour
 {
+    [Header("Skills CoolDown")]
     [SerializeField] private Button dashSkill;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void DashSkill(bool activeState)
     {
